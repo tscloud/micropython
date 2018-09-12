a81d76eabf8dc390f0480a4d688be4bd8c6ed957
tscloud <thomas.s.cloud@gmail.com>
2018-09-12 15:35:16 -0400

    Try picoweb templates, display dummy data

--- picoweb_test.py
+++ picoweb_test.py
@@ -4,7 +4,18 @@ app = picoweb.WebApp(__name__)
 
 @app.route('/')
 def index(req, resp):
-    yield from picoweb.start_response(resp)
-    yield from resp.awrite('Hello world from picoweb running on the ESP32')
+    t = 123
+    h = 456
+    p = 789
+    templateData = {
+        'temp' : t,
+        'humidity': h,
+        'pressure': p
+
+    }
+    #yield from picoweb.start_response(resp)
+    yield from picoweb.start_response(resp, content_type = "text/html")
+    #yield from resp.awrite('Hello world from picoweb running on the ESP32')
+    yield from app.render_template(resp, "sensor.tpl", (templateData,))
 
 app.run(debug=True, host='especial_1')
